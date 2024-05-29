4846bda4967937e3ce6111fc9bd0d8d916871af8
mehtaamisha <142864251+mehtaamisha@users.noreply.github.com>
2024-05-29 00:02:04 -0400

    home page coming soon changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import coming_soon_img from './assets/coming-soon.gif'
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Home></Home>
+    <div className='App'>
+      {/* <Navbar></Navbar>
+      <Home></Home> */}
+      <div className="content">
+        <img src={coming_soon_img} alt="Website Coming Soon..." style={{ height: "500px" }} />
+        <h1 style={{
+          color: '#333',
+          fontFamily: 'Arial, sans-serif',
+          fontWeight: 'bold',
+          marginTop: '20px',
+          fontSize: '24px'
+        }}>Hello!! I am Amisha Mehta. Website is Coming up Soon.</h1>
+      </div>
     </div>
   );
 }
